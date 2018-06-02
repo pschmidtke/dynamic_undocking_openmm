8141084f8a22036ca8affd07c81d5f349c81f9c6
Peter Schmidtke <peter@Mac-Pro.local>
2018-06-02 15:15:43 +0200

    commiting function version for 3ert

--- 3ert/mergeTry1.py
+++ 3ert/mergeTry1.py
@@ -77,7 +77,10 @@ def generateSMIRNOFFStructureRDK(molecule):
 	return molecule_structure
 
 
-lig_rdk=Chem.MolFromMol2File('ligand_centered.mol2',sanitize=False)
+ligand_file='ligand.mol2'
+protein_file='3ert.pdb'
+
+lig_rdk=Chem.MolFromMol2File(ligand_file,sanitize=False)
 
 
 #m=Chem.MolFromSmiles('CC\C(=C(/c1ccc(O)cc1)c1ccc(OCCN(C)C)cc1)c1ccccc1',sanitize=True)
@@ -85,15 +88,15 @@ lig_rdk=Chem.MolFromMol2File('ligand_centered.mol2',sanitize=False)
 #AllChem.EmbedMolecule(lig_rdk, AllChem.ETKDG())
 lig_rdk.SetProp('_Name','LIG')
 
-molecule_structure=generateSMIRNOFFStructureRDK(lig_rdk)
+ligand_pmd=generateSMIRNOFFStructureRDK(lig_rdk)
 
 
 
 
-print("saving ligand topology")
+#print("saving ligand topology")
 
-molecule_structure.save("ligand.pdb",overwrite=True)
-molecule_structure.save("ligand.prmtop", overwrite=True)
+#ligand_pmd.save("ligand.pdb",overwrite=True)
+#ligand_pmd.save("ligand.prmtop", overwrite=True)
 
 
 
@@ -107,7 +110,61 @@ molecule_structure.save("ligand.prmtop", overwrite=True)
 #f.close()
 
 print("loading system")
-receptor=parmed.load_file("system.prmtop",xyz="system.inpcrd")
+protein=parmed.load_file(protein_file)
+protein = protein["!(:HOH,NA,CL)"] #remove ions and water
+
+forcefield = app.ForceField('amber99sb.xml')
+
+protein_system = forcefield.createSystem(protein.topology)
+
+protein_pmd = parmed.openmm.load_topology(protein.topology, protein_system, protein.positions)
+print(protein_pmd)
+
+
+prot_lig_pmd = protein_pmd + ligand_pmd
+
+
+print("Solvation")
+fixer = PDBFixer('3ert_orig.pdb')
+
+
+#0.1 in Vec3 because box_size is in angstrom and fixer uses nanometer
+#scaling factor to somehow ensure no interaction with periodic image 
+scaling_factor = 1.0
+box_size=50
+fixer.addSolvent(scaling_factor * box_size *openmm.Vec3(0.1, 0.1, 0.1), positiveIon='Na+', negativeIon='Cl-', ionicStrength=0.1*unit.molar)
+
+app.PDBFile.writeFile(fixer.topology, fixer.positions, open('3ert_solvated.pdb', 'w'))
+
+print("Solvation done")
+print("Parametrizing ions")
+
+complex = parmed.load_file('./complex_solvated.pdb')
+
+ions = complex["(:NA,CL)"]
+
+
+forcefield = app.ForceField('amber99sb.xml')
+ions_system = forcefield.createSystem(ions.topology)
+# print(solvent_system.getNumConstraints())
+
+ions_pmd = parmed.openmm.load_topology(ions.topology, ions_system, ions.positions)
+print("Parametrizing ions done")
+print("Parametrizing solvent")
+
+solvent = complex["(:HOH)"]
+num_solvent = len(solvent.residues)
+# # This does not currently work for water
+# forcefield = app.ForceField('amber99sb.xml', 'tip3p.xml', 'residues.xml')
+# solvent_system = forcefield.createSystem(solvent.topology, nonbondedMethod=app.PME,
+#             nonbondedCutoff=1*unit.nanometer, constraints=HBonds, rigidwater = True , flexibleConstraints=True)
+# print(solvent_system.getNumConstraints())
+
+
+solvent_pmd = parmed.load_file("../water/water.prmtop")
+solvent_pmd *= num_solvent
+solvent_pmd.positions = solvent.positions
+print("Parametrizing solvent done")
 
 
 #proteinpdb = app.PDBFile('system_solvated.pdb')
@@ -116,32 +173,46 @@ receptor=parmed.load_file("system.prmtop",xyz="system.inpcrd")
 
 #protein_structure = utils.generateProteinStructure(proteinpdb)
 print("merge structures")
-structure = utils.mergeStructure( molecule_structure,receptor)
-print("save structures")
-structure.save("system_complex.pdb",overwrite=True)
-structure.save("system_complex.prmtop", overwrite=True)
+
+combined_pmd = prot_lig_pmd + ions_pmd + solvent_pmd
+combined_pmd.box_vectors = complex.box_vectors
+print(complex.box_vectors)
+
+combined_pmd.save("system_complex.prmtop", overwrite=True)
+
+combined_system = combined_pmd.createSystem(nonbondedMethod=app.CutoffPeriodic, 
+                               nonbondedCutoff=1*unit.nanometer, constraints=app.AllBonds)
+
+print("Setting up simulation")
+
+temp = 300 # needs to run simulation in several stages, otherwise explodes
 
 
-print("create system")
-#system=protein_structure.createSystem(constraints=None, rigidWater=None,flexibleConstraints=None)
-system=structure.createSystem(nonbondedMethod=app.PME,nonbondedCutoff=8*unit.angstrom)
+integrator = openmm.VerletIntegrator(0.002 * unit.picoseconds)
+thermostat = openmm.AndersenThermostat(temp * unit.kelvin, 1/unit.picosecond)
+combined_system.addForce(thermostat)
+barostat = openmm.MonteCarloBarostat(1.013 * unit.bar, 300 * unit.kelvin)
+combined_system.addForce(barostat)
 
-print("setting up md")
-integrator = openmm.LangevinIntegrator(300*unit.kelvin,1/unit.picosecond, 0.002*unit.picoseconds)
-simulation = app.Simulation(structure.topology, system, integrator)
-simulation.context.setPositions(structure.positions)	
-simulation.minimizeEnergy()
+
+
+platform = openmm.Platform.getPlatformByName('OpenCL')
+simulation = app.Simulation(combined_pmd.topology, combined_system, integrator, platform)
+
+if combined_pmd.box_vectors is not None:
+    simulation.context.setPeriodicBoxVectors(*combined_pmd.box_vectors)
+simulation.context.setPositions(combined_pmd.positions)
+
+
+
+
+
 simulation.reporters.append(app.DCDReporter("output.dcd",10))
-simulation.step(10000)
-
-
-mol_top, mol_sys, mol_pos=generateSMIRNOFFSystemRDK(lig_rdk)
-integrator = openmm.LangevinIntegrator(300*unit.kelvin,1/unit.picosecond, 0.002*unit.picoseconds)
-simulation = app.Simulation(mol_top, mol_sys, integrator)
-simulation.context.setPositions(mol_pos)
 simulation.minimizeEnergy()
-simulation.reporters.append(app.DCDReporter("output2.dcd",10))
-simulation.step(10000)
+simulation.step(100000)
+
+
+sys.exit("Done")
 
 
 
